7952f48d1e3e8127991d15c6e17d2dbd9a2c73da
EDDChang <mrchang87@gmail.com>
2023-06-08 17:29:32 +0800

    only one add can work now

--- test_client.py
+++
@@ -1,7 +0,0 @@
-from multiprocessing.connection import Client
-from array import array
-
-address = ('localhost', 6000)
-
-with Client(address, authkey=b'secret password') as conn:
-    conn.send({"test": "test"})
